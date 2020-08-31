b43022f078788c8654685ff9a4ae8ba8b7ab90bc
cccy <cccy0@foxmail.com>
2020-09-01 00:22:16 +0800

    :tada: init

---
+++ mall-tiny-01/src/test/java/com/cccy/mall/tiny/MallApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cccy.mall.tiny;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MallApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
